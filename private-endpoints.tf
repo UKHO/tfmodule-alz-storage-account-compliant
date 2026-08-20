9a5d97c996335191996a4aebed9cad9a3a331495
Luke Duddridge <205003513+ld-at-work@users.noreply.github.com>
2026-08-20 10:21:39 +0100

    Changing properties to use id

--- private-endpoints.tf
+++ private-endpoints.tf
@@ -117,8 +117,7 @@ resource "azurerm_private_endpoint" "file_secondary" {
 resource "azurerm_private_dns_zone_virtual_network_link" "blob_primary" {
   count                 = var.create_primary_dns_vnet_links && var.enable_primary_private_endpoints ? 1 : 0
   name                  = "${var.virtual_network_name}-blob-link"
-  resource_group_name   = var.oldhub_dns_zone_resource_group
-  private_dns_zone_name = data.azurerm_private_dns_zone.storage_blob[0].name
+  private_dns_zone_id   = data.azurerm_private_dns_zone.storage_blob[0].id
   virtual_network_id    = data.azurerm_virtual_network.main[0].id
   registration_enabled  = false
 
@@ -132,8 +131,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "blob_primary" {
 resource "azurerm_private_dns_zone_virtual_network_link" "file_primary" {
   count                 = var.create_primary_dns_vnet_links && var.enable_primary_private_endpoints ? 1 : 0
   name                  = "${var.virtual_network_name}-file-link"
-  resource_group_name   = var.oldhub_dns_zone_resource_group
-  private_dns_zone_name = data.azurerm_private_dns_zone.storage_file[0].name
+  private_dns_zone_id = data.azurerm_private_dns_zone.storage_file[0].id
   virtual_network_id    = data.azurerm_virtual_network.main[0].id
   registration_enabled  = false
 
