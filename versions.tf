875d4a52ff3e3b42c2770bd98b11f7af7982dd60
Luke Duddridge <205003513+ld-at-work@users.noreply.github.com>
2026-08-20 13:35:45 +0100

    setting version to 5

--- versions.tf
+++ versions.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = ">= 3.70"
+      version               = ">= 5.0.0"
       configuration_aliases = [azurerm.hub, azurerm.oldhub]
     }
     azapi = {
